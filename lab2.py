b2c8fdcb921406d16f27ef738651604e8fa1425d
Luis Zaman <zamanlh@umich.edu>
2021-02-08 17:34:11 -0500

    added lab and updated pdf

---
+++ lab2.py
@@ -0,0 +1,31 @@
+###############################################
+### CMPLXSYS425 - Evolution in silico       ###
+### Lab 2                                   ###
+### ####################################### ###
+### IF YOU HAVEN'T LOOKED AT THE NOTEBOOKS  ###
+### ON GITHUB FIRST, PLEASE DO THAT!        ###
+###############################################
+
+#pulling this over from the Jupyter notebook
+pop_size = 20
+num_mutants = 5
+num_generations = 10
+
+mutation_frequency_list = []
+population = np.zeros(pop_size)
+
+for i in range(num_mutants):
+    population[i] = 1
+
+#Let's be sure to record the frequency at the beginning of time!
+mutation_frequency_list.append(mutant_freq(population))
+
+for gen in range(num_generations):
+    population = np.random.choice(population, pop_size)
+    print("Generation: {0}, MutFreq: {1}".format(gen+1, mutant_freq(population)))
+    mutation_frequency_list.append(mutant_freq(population))
+
+plt.plot(mutation_frequency_list)
+plt.ylabel("Mutation Freq")
+plt.xlabel("Generation")
+plt.show()
